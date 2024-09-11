687b44a05d730631bc0a117edff95f0564ea5e26
Bogdan Marchulov <bogdan_info@mail.ru>
2024-09-11 15:40:10 +0300

    feat: update

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -5,11 +5,12 @@ return {
     require("auto-save").setup({
       enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
       execution_message = {
+        disabling = true,
         message = function() -- message to print on save
           return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
         end,
         dim = 0.18, -- dim the color of `message`
-        cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+        cleaning_interval = 100, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
       },
       trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
       -- function that determines whether to save the current buffer or not
